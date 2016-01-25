252e273a54c4707bcf98482a9f9a84fd232d07f1
Jason Awbrey and Will Johnson <jason.s.awbrey@gmail.com>
2016-01-25 09:53:03 -0800

    Add a working constructor and UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
+function Place (city, state, landmarks, season, notes){
+  this.location = [city, state];
+  this.landmarks = landmarks;
+  this.season = season;
+  this.notes = notes;
+}
+
+Place.prototype.locationName = function () {
+  return this.location[0] + ", " + this.location[1];
+}
+
+
+$(function(){
+  $('form#new-place').submit(function(event){
+
+    var userCity = $('#new-city').val();
+    var userState = $('#new-state').val();
+    var userLandmarks = $('#new-landmarks').val();
+    var userSeason = $('#new-season').val();
+    var userNotes = $('#new-notes').val();
+
+    var visitedPlace = new Place (userCity, userState, userLandmarks, userSeason, userNotes);
+
+    $('ul#places-list').append("<li><span class='location'>"+visitedPlace.locationName()+"</span></li>");
+
+    event.preventDefault();
+
+  $('.location').last().click(function() {
+    $('#show-place').show();
+    $('#location').text(visitedPlace.locationName());
+    $('#landmarks').text(visitedPlace.landmarks);
+    $('#season').text(visitedPlace.season);
+    $('#notes').text(visitedPlace.notes);
+  });
+});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,14 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe("Place", function(){
+  it("Will make a new place object", function(){
+    var portland = new Place ("Portland", "Oregon","Hawthorne Bridge", "Summer", "Very nice.");
+    expect(portland.location).to.eql(["Portland", "Oregon"]);
+    expect(portland.landmarks).to.equal("Hawthorne Bridge");
+    expect(portland.season).to.equal("Summer");
+    expect(portland.notes).to.equal("Very nice.");
+  });
+
+  it("Will return a full location name", function(){
+    var portland = new Place ("Portland", "Oregon","Hawthorne Bridge", "Summer", "Very nice.");
+    expect(portland.locationName()).to.equal("Portland, Oregon");
+  });
+});
